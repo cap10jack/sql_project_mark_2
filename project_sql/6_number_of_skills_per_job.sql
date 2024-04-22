105deb2163b7a3d602c4b4ca56ae24e143707288
cap10jack <166293704+cap10jack@users.noreply.github.com>
2024-04-22 13:34:10 +0530

    Update 6_number_of_skills_per_job.sql

--- project_sql/6_number_of_skills_per_job.sql
+++ project_sql/6_number_of_skills_per_job.sql
@@ -1,5 +1,7 @@
--- number of skills required per job
-
+/* number of skills required per job
+6. Question: Optimizing for least skills to know for a higher salary
+-  we will need the count function for the number of skills
+*/
 
    with skillcount as  
 (select job_postings_fact.job_id,job_postings_fact.job_title_short,
@@ -7,9 +9,7 @@
 from skills_job_dim 
 inner join job_postings_fact on
            skills_job_dim.job_id = job_postings_fact.job_id
-           inner join company_dim
-on job_postings_fact.company_id = company_dim.company_id
-
+        
             where salary_year_avg IS NOT NULL
 
 group by job_postings_fact.job_id
